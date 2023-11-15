a521961182adeca3b30f6c3de1459ca30f7ed4fd
Tortuga465 <Tem9669@yandex.ru>
2023-11-15 19:34:49 +0300

    Added create files and hash

---
+++ create_files.py
@@ -0,0 +1,26 @@
+import lorem
+import os.path
+import hashlib
+
+
+def crate_block_chain():
+    files_amount=int(input("enter number of files\n"))                              
+    rel_path_dir=os.getcwd()                                                        # Относительный путь. На Линукс не будет работать, т.к. формат пути другой
+      # Путь до файла
+    
+    for i in range(1, files_amount+1):
+        rel_path_file=os.path.join(rel_path_dir,"text_files\chain{}".format(i)+".txt")
+        f=open(rel_path_file,"w")
+        md5_hash=""
+        if(i>1):
+            with open(os.path.join(rel_path_dir,"text_files\chain{}".format(i-1)+".txt"), 'rb') as file_obj:
+                file_contents = file_obj.read()
+                md5_hash = hashlib.md5(file_contents).hexdigest()
+                f.write(md5_hash)
+                f.write("\n")
+        f.write(lorem.sentence())
+        f.close
+        
+
+
+
---
+++ main.py
@@ -0,0 +1,3 @@
+import create_files
+
+create_files.crate_block_chain()
