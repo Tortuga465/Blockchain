37d005bcca5fdbf9e1031077d146f4c26b8730ed
Tortuga465 <Tem9669@yandex.ru>
2023-11-15 20:23:45 +0300

    fixed natural sort

--- check_hash.py
+++ check_hash.py
@@ -4,10 +4,13 @@ import natsort as ns
 
 
 def check_hashes(rel_path_dir):
-    rel_path_dir=rel_path_dir+r"\text_files".strip()
-    file_list=os.listdir(rel_path_dir)[1:]          #removes .gitignore
-    print(ns.natsorted(file_list))
-    for filename in file_list[2:]:
+    rel_path_dir=rel_path_dir+r"\text_files".strip()    #Записывает директорию с блоками
+    file_list=os.listdir(rel_path_dir)[1:]              #Список файлов без .gitignore
+    file_list=ns.natsorted(file_list)                      #Натуральная сортировка  массива
+    hashes=[]
+    for filename in file_list[1:]:
         if filename.endswith('.txt'):
             with open(os.path.join(rel_path_dir, filename)) as f:
-                print(f.readline().strip())
+                hash_line=f.readline().strip()
+                hashes.append(hash_line)
+    print(hashes)
