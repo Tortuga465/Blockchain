ee5826247093c3822c2de6417f5240bef0597db6
MarkXXIX <Volkir296@users.noreply.github.com>
2023-12-11 18:46:11 +0300

    Создание функции create_file

--- create_files.py
+++ create_files.py
@@ -2,15 +2,35 @@ import lorem
 import os.path
 import hashlib
 
+folder_dir = "\\text_files\\" # Нахождение в папке с создаваемыми файлами
 
-def create_block_chain(files_amount, rel_path_dir):
-                             
-# Относительный путь. На Линукс не будет работать, т.к. формат пути другой
-      # Путь до файла
+#Функция создания файлов
+def create_file(files_amount: int, rel_path_dir: str): #Функция принимает два параметра: files_amount (количество файлов) и rel_path_dir (относительный путь к директории).
     
-    for i in range(1, files_amount+1):
-        rel_path_file=os.path.join(rel_path_dir,"text_files\chain{}".format(i)+".txt")
+    names_files = list() #Записывает имена файлов
+
+    for i in range(1, files_amount+1): #Выполняется цикл от 1 до files_amount+1.
+        templ_name = f"chain{i}.txt" #Шаблон имени файла
+
+        #Создание файла/ов
+        f=open(rel_path_dir+folder_dir+templ_name,"w") 
+        f.close()
+
+        #Добавление в лист имён
+        names_files.append(templ_name)
+
+    return names_files #Возрат имён файлов в main
+        
+
+    
+
+def create_block_chain(files_amount: int, rel_path_dir: str):              #name_list: list    
+    
+    for i in range(1, files_amount+1): #Выполняется цикл от 1 до files_amount+1.
+        rel_path_file=os.path.join(rel_path_dir,"text_files\chain{}".format(i)+".txt") #Создается относительный путь к файлу, используя функцию os.path.join и значение i.
         f=open(rel_path_file,"w")
+        
+
         md5_hash=""
         if(i>1):
             with open(os.path.join(rel_path_dir,"text_files\chain{}".format(i-1)+".txt"), 'rb') as file_obj:
