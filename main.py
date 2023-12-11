853e5ac5db9d3a5befdf5ec7813deee0a24c1492
MarkXXIX <Volkir296@users.noreply.github.com>
2023-12-11 16:41:54 +0300

    UDP Main

--- main.py
+++ main.py
@@ -2,12 +2,19 @@ import create_files
 import check_hash
 import os
 
-# Инициализация
-files_amount=int(input("Введите количество файлов\n"))
-rel_path_dir=os.getcwd()
-# Инициализация
+def main():
 
-#Функция создает блокчейн
-create_files.crate_block_chain(files_amount, rel_path_dir)
-#Функция проверяет блокчейн
-#check_hash.check_hashes(rel_path_dir)
+    file_names = [] #Список имён файлов
+
+    files_amount = int(input("Введите количество файлов\n")) #количество файлов
+    rel_path_dir = os.getcwd() #количество файлов
+    
+    #Функция создает блокчейн
+    #create_files.crate_block_chain(files_amount, rel_path_dir)
+    #Функция проверяет блокчейн
+    #check_hash.check_hashes(rel_path_dir)
+
+
+
+if __name__ == '__main__':
+    main()
