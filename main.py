3144fc635714ff9187b0c41236c9b0d5f88628fb
Tortuga465 <Tem9669@yandex.ru>
2023-12-19 01:11:23 +0300

    добавил команды cli чтобы не комментить код

--- main.py
+++ main.py
@@ -4,23 +4,28 @@ import os
 
 def main():
 
+    action="" # Выбор действия пользователя: создание, проверка, выход
     files_names = [] #Список имён файлов
-
-    files_amount = int(input("Введите количество файлов\n")) #Количество файлов
     rel_path_dir = os.getcwd() #Путь
+
+    while ("check" != action and "create"!=action and "exit"!=action):
+        action=input("enter action: \ncreate - создание блокчейна\ncheck - проверка блокчейна на валдиность\nexit - выход из программы\n")
     
-    #Функция создает файлы
-    files_names = create_file(files_amount, rel_path_dir) 
-    #print(len(files_names), files_names)  #Проверка функции
+    if(action=="create"):
+        files_amount = int(input("Введите количество файлов\n")) #Количество файлов
+        #Функция создает файлы
+        files_names = create_file(files_amount, rel_path_dir) 
+        print(len(files_names), files_names)  #Проверка функции
+        
+        #Функция создаёт цепочки
+        create_block_chain(files_names, rel_path_dir)
     
-    #Функция создаёт цепочки
-    create_block_chain(files_names, rel_path_dir)
+    if(action=="check"):
+        #Функция проверяет блокчейн
+        check_hashes(rel_path_dir)
 
-
-    #Функция проверяет блокчейн
-    #check_hash.check_hashes(rel_path_dir)
-
-
-
+    if(action=="exit"):
+        exit
+        
 if __name__ == '__main__':
     main()
